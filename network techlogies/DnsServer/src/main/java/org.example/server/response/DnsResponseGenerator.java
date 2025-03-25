cc2cfcd2576702f032aab8d859a26325519a7da5
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-03-25 12:39:42 +0700

    Create DnsResponseGenerator.java

---
+++ network techlogies/DnsServer/src/main/java/org.example/server/response/DnsResponseGenerator.java
@@ -0,0 +1,36 @@
+package org.example.server.response;
+
+import org.example.server.request.DiscoverDnsRequest;
+import org.example.server.request.DnsRequest;
+import org.example.server.request.QueryDnsRequest;
+import org.example.server.request.RegisterDnsRequest;
+import org.example.util.DnsException;
+
+import java.net.InetAddress;
+import java.util.HashMap;
+
+public class DnsResponseGenerator {
+    private final InetAddress dnsServerAddress;
+    private final HashMap<String, String> domainTable;
+    public DnsResponseGenerator(
+            InetAddress dnsServerAddress,
+            HashMap<String, String> domainTable
+    ) {
+        this.dnsServerAddress = dnsServerAddress;
+        this.domainTable = domainTable;
+    }
+
+    public DnsResponse getDnsResponse(DnsRequest request) {
+        return switch (request) {
+            case DiscoverDnsRequest ignore -> new DiscoverDnsResponse(dnsServerAddress);
+            case QueryDnsRequest queryDnsRequest -> new QueryDnsResponse(domainTable.get(queryDnsRequest.domainName()));
+            case RegisterDnsRequest registerDnsRequest -> {
+                if (domainTable.containsKey(registerDnsRequest.domainName())) {
+                    throw new DnsException("Duplicate domain name: " + registerDnsRequest.domainName());
+                }
+                domainTable.put(registerDnsRequest.domainName(), registerDnsRequest.address());
+                yield new RegisterDnsResponse(registerDnsRequest.domainName(), registerDnsRequest.address());
+            }
+        };
+    }
+}
