694c64fdec5e863e2852ce9320d3ea55155c080e
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-04-07 12:55:09 +0700

    Create Contexts.java

---
+++ oop java/task-2/lab2/context/src/main/java/org.example/Contexts.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public interface Contexts {
+    Stack<Double> getStack();
+    void newDefine(Character name, Double num);
+    Double getVal(Character name);
+    Map<Character, Double> getDefines();
+    void addLine(List<String> newLine);
+    List<String> getLine();
+}
