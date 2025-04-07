591448012acbef8ff6439dd75baf5300ee801be0
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-04-07 12:24:07 +0700

    Create Plus.java

---
+++ oop java/task-2/lab2/commands/src/main/java/org.example/Plus.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+public class Plus implements Command {
+    @Override
+    public void operation(Contexts context) {
+        if (context.getStack().size() < 2) {
+            throw new IllegalStateException("Not enough operands for addition");
+        }
+        Double a = context.getStack().pop();
+        Double b = context.getStack().pop();
+        context.getStack().push(a + b);
+    }
+}
