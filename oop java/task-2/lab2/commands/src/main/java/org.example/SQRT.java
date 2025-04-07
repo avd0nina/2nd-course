61ba818b4093666dd3030acaeb9031bba18c1b00
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-04-07 12:25:04 +0700

    Create SQRT.java

---
+++ oop java/task-2/lab2/commands/src/main/java/org.example/SQRT.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class SQRT implements Command {
+    public void operation(Contexts context){
+        if (context.getStack().isEmpty()) {
+            throw new IllegalStateException("Not enough operands for SQRT");
+        }
+        Double a = context.getStack().pop();
+        context.getStack().push(Math.sqrt(a));
+    }
+}
