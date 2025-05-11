85e65e575836b42c43d7b9b9dde9e6217f7743b5
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-05-12 01:16:58 +0700

    Create DealersCreator.java

---
+++ oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/carsale/DealersCreator.java
@@ -0,0 +1,34 @@
+package ru.nsu.avdonina.model.work.carsale;
+
+import ru.nsu.avdonina.model.listeners.CarRequestListener;
+import ru.nsu.avdonina.model.products.Car;
+import ru.nsu.avdonina.model.storage.Storage;
+import ru.nsu.avdonina.model.work.WorkersCreator;
+import ru.nsu.avdonina.model.work.factory.FactoryPlanner;
+
+import java.util.logging.Logger;
+
+public class DealersCreator extends WorkersCreator<Dealer> { // фабрика для создания потоков-продавцов автомобилей
+    private final Storage<Car> storage;
+    private final CarRequestListener carRequestListener;
+    private final FactoryPlanner factoryPlanner;
+    private final Logger saleLogger;
+
+    public DealersCreator(Storage<Car> storage,
+                          CarRequestListener carRequestListener,
+                          FactoryPlanner factoryPlanner,
+                          Logger saleLogger,
+                          int workTime) {
+
+        super(workTime);
+        this.storage = storage;
+        this.carRequestListener = carRequestListener;
+        this.factoryPlanner = factoryPlanner;
+        this.saleLogger = saleLogger;
+    }
+
+    @Override
+    public Dealer createWorker() {
+        return new Dealer(storage, carRequestListener, factoryPlanner, saleLogger, getWorkTime());
+    }
+}
