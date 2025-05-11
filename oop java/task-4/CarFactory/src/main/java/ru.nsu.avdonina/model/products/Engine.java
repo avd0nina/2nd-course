80961ef9ef0294f20a82669dc9392c58b729e2c8
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-05-12 01:13:41 +0700

    Create Engine.java

---
+++ oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/products/Engine.java
@@ -0,0 +1,15 @@
+package ru.nsu.avdonina.model.products;
+
+public class Engine implements CarPart {
+    private final Id id = Id.createEngineId();
+
+    @Override
+    public int getId() {
+        return id.getIntegerId();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Engine-%d", getId());
+    }
+}
