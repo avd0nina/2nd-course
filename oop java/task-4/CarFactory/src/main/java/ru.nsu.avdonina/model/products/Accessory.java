917a0ef95a9c2fd6fe9ff86b32c7ec8f00f1e53b
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-05-12 01:12:12 +0700

    Create Accessory.java

---
+++ oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/products/Accessory.java
@@ -0,0 +1,15 @@
+package ru.nsu.avdonina.model.products;
+
+public class Accessory implements CarPart {
+    private final Id id = Id.createAccessoryId();
+
+    @Override
+    public int getId() {
+        return id.getIntegerId();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Accessory-%d", getId());
+    }
+}
