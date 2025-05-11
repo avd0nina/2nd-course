1c689358949b06febf82802218a0ef8a34fe126a
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-05-12 01:05:41 +0700

    Create PropertiesException.java

---
+++ oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/parsing/PropertiesException.java
@@ -0,0 +1,15 @@
+package ru.nsu.avdonina.model.parsing;
+
+public class PropertiesException extends Exception {
+    public PropertiesException(String message) {
+        super(message);
+    }
+
+    public PropertiesException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PropertiesException(Throwable cause) {
+        super(cause);
+    }
+}
