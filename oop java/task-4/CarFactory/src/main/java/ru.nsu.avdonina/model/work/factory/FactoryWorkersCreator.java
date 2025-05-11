d638a8dc7b1309d820e782adc7c4b2cee06a1b74
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-05-12 01:18:55 +0700

    Create FactoryWorkersCreator.java

---
+++ oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/factory/FactoryWorkersCreator.java
@@ -0,0 +1,56 @@
+package ru.nsu.avdonina.model.work.factory;
+
+import ru.nsu.avdonina.model.listeners.CarAssembleListener;
+import ru.nsu.avdonina.model.listeners.FactoryTasksListener;
+import ru.nsu.avdonina.model.products.Accessory;
+import ru.nsu.avdonina.model.products.Body;
+import ru.nsu.avdonina.model.products.Car;
+import ru.nsu.avdonina.model.products.Engine;
+import ru.nsu.avdonina.model.storage.Storage;
+import ru.nsu.avdonina.model.work.WorkersCreator;
+
+public class FactoryWorkersCreator extends WorkersCreator<FactoryWorker> {
+    private final Object workersSynchronizationObject;
+    private final Storage<Body> bodyStorage;
+    private final Storage<Engine> engineStorage;
+    private final Storage<Accessory> accessoryStorage;
+    private final Storage<Car> carStorage;
+    private final CarAssembleListener carAssembleListener;
+    private final FactoryTasksListener factoryTasksListener;
+
+    public FactoryWorkersCreator(Storage<Body> bodyStorage,
+                                 Storage<Engine> engineStorage,
+                                 Storage<Accessory> accessoryStorage,
+                                 Storage<Car> carStorage,
+                                 CarAssembleListener carAssembleListener,
+                                 FactoryTasksListener factoryTasksListener,
+                                 int workTime) {
+
+        super(workTime);
+        this.workersSynchronizationObject = new Object();
+        this.bodyStorage = bodyStorage;
+        this.engineStorage = engineStorage;
+        this.accessoryStorage = accessoryStorage;
+        this.carStorage = carStorage;
+        this.carAssembleListener = carAssembleListener;
+        this.factoryTasksListener = factoryTasksListener;
+    }
+
+    public Object getWorkersSynchronizationObject() {
+        return workersSynchronizationObject;
+    }
+
+    @Override
+    public FactoryWorker createWorker() {
+        return new FactoryWorker(
+                getWorkTime(),
+                bodyStorage,
+                engineStorage,
+                accessoryStorage,
+                carStorage,
+                carAssembleListener,
+                factoryTasksListener,
+                workersSynchronizationObject
+        );
+    }
+}
