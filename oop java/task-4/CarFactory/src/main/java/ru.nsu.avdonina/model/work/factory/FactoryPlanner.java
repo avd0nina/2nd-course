c57135079ba251cb26e28c59191bdd149d9df579
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-05-13 09:53:14 +0700

    Update FactoryPlanner.java

--- oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/factory/FactoryPlanner.java
+++ oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/factory/FactoryPlanner.java
@@ -3,7 +3,7 @@ package ru.nsu.avdonina.model.work.factory;
 public abstract class FactoryPlanner extends Thread {
     private final Factory factory;
     private final Object synchronizationObject = new Object();
-    private boolean wasNotified = false;
+    private volatile boolean wasNotified = false;
 
     protected FactoryPlanner(Factory factory) {
         this.factory = factory;
@@ -11,29 +11,35 @@ public abstract class FactoryPlanner extends Thread {
 
     @Override
     public void run() {
-        while (true) {
-            final int newTasksCount = getNewTasksCount();
-            factory.addTasks(newTasksCount);
+        while (!isInterrupted()) {
+            int newTasksCount = getNewTasksCount();
+            if (newTasksCount > 0) {
+                factory.addTasks(newTasksCount);
+            }
             wasNotified = false;
-            while (!wasNotified) {
-                synchronized (synchronizationObject) { // cинхронизируем блок кода чтобы гарантировать, что только один поток будет выполнять блок внутри синхронизации в каждый момент времени
+
+            synchronized (synchronizationObject) {
+                while (!wasNotified) {
                     try {
-                        synchronizationObject.wait(); // поток, который вызвал этот метод, будет приостановлен до тех пор, пока не получит сигнал от другого потока
-                    } catch (InterruptedException exception) {
+                        synchronizationObject.wait();
+                    } catch (InterruptedException e) {
                         interrupt();
                         return;
                     }
                 }
             }
         }
-    } // Когда рабочие завершат выполнение задач или когда возникнет другая ситуация, планировщик получит сигнал (с помощью метода notify()), и этот блок кода завершит ожидание
+    }
 
-    public Object getSynchronizationObject() { // Когда рабочие завершат выполнение задач или когда возникнет другая ситуация, планировщик получит сигнал (с помощью метода notify()), и этот блок кода завершит ожидание
+    public Object getSynchronizationObject() {
         return synchronizationObject;
     }
 
-    public void notifyAboutCarSale() { // была произведена продажа автомобиля, и планировщик теперь может начать планировать следующие задачи
+    public void notifyAboutCarSale() {
         wasNotified = true;
+        synchronized (synchronizationObject) {
+            synchronizationObject.notifyAll();
+        }
     }
 
     public abstract int getNewTasksCount();
