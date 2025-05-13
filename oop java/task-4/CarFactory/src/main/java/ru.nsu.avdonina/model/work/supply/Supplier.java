259714c7ce54914fae1d1aabe2fbb85849675c37
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-05-13 09:56:22 +0700

    Update Supplier.java

--- oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/supply/Supplier.java
+++ oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/supply/Supplier.java
@@ -3,9 +3,9 @@ package ru.nsu.avdonina.model.work.supply;
 import ru.nsu.avdonina.model.listeners.SupplyListener;
 import ru.nsu.avdonina.model.products.CarPart;
 import ru.nsu.avdonina.model.storage.Storage;
-import ru.nsu.avdonina.model.work.factory.Worker;
+import ru.nsu.avdonina.model.work.Worker;
 
-public abstract class Supplier<P extends CarPart> extends Worker { // каждый поставщик будет работать в отдельном потоке
+public abstract class Supplier<P extends CarPart> extends Worker {
     private final Storage<P> storage;
     private final SupplyListener supplyListener;
 
@@ -17,18 +17,14 @@ public abstract class Supplier<P extends CarPart> extends Worker { // кажды
 
     @Override
     public void run() {
-        while (!isSoftlyInterrupted()) {
-            try {
-                Thread.sleep(getWorkTime());
-                final P carPart = supply();
-                storage.put(carPart);
-                supplyListener.notifyAboutSupply(carPart);
-            } catch (InterruptedException exception) {
-                interrupt();
-                return;
-            }
+        try {
+            Thread.sleep(workTime);
+            P carPart = supply();
+            storage.put(carPart);
+            supplyListener.notifyAboutSupply(carPart);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
-        interrupt();
     }
 
     protected abstract P supply();
