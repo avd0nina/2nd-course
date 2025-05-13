44f1c4369aacb87dda5e7e9930c4fefc06a4323a
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-05-13 09:50:50 +0700

    Update Dealer.java

--- oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/carsale/Dealer.java
+++ oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/carsale/Dealer.java
@@ -3,8 +3,8 @@ package ru.nsu.avdonina.model.work.carsale;
 import ru.nsu.avdonina.model.listeners.CarRequestListener;
 import ru.nsu.avdonina.model.products.Car;
 import ru.nsu.avdonina.model.storage.Storage;
+import ru.nsu.avdonina.model.work.Worker;
 import ru.nsu.avdonina.model.work.factory.FactoryPlanner;
-import ru.nsu.avdonina.model.work.factory.Worker;
 
 import java.util.logging.Logger;
 
@@ -21,8 +21,8 @@ public class Dealer extends Worker {
                   Logger logger,
                   int carRequestTime) {
         super(carRequestTime);
-        this.factoryPlanner = factoryPlanner;
         this.storage = storage;
+        this.factoryPlanner = factoryPlanner;
         this.logger = logger;
         this.carRequestListener = carRequestListener;
         this.factoryPlannerSynchronizationObject = factoryPlanner.getSynchronizationObject();
@@ -30,24 +30,25 @@ public class Dealer extends Worker {
 
     @Override
     public void run() {
-        while (!isSoftlyInterrupted()) {
-            try {
-                final Car car = storage.take(); // Берет одну машину со склада
-                carRequestListener.notifyAboutCarSale(); // Уведомляет о продаже
-                if (logger != null) {
-                    final String message = String.format("Dealer %s: %s%n", Thread.currentThread().getName(), car); // пишет информацию в лог: кто продал какую машину
-                    logger.info(message);
-                }
-                Thread.sleep(getWorkTime()); // эмуляция времени продажи
-                synchronized (factoryPlannerSynchronizationObject) { // Поток входит в синхронизированный блок
-                    factoryPlanner.notifyAboutCarSale(); // Вызов notifyAboutCarSale() устанавливает флаг wasNotified = true в FactoryPlanner
-                    factoryPlannerSynchronizationObject.notifyAll(); // notifyAll() пробуждает FactoryPlanner, который спал в ожидании события продажи машины
-                } // После этого FactoryPlanner снова запускает getNewTasksCount() и добавляет задачи на сборку новых машин
-            } catch (InterruptedException exception) {
-                interrupt();
-                return;
+        try {
+            Car car = storage.take(); // Блокируется, если машин нет
+            carRequestListener.notifyAboutCarSale();
+
+            if (logger != null) {
+                String message = String.format("Dealer %s: %s%n", Thread.currentThread().getName(), car);
+                logger.info(message);
             }
+
+            Thread.sleep(workTime);
+
+            synchronized (factoryPlannerSynchronizationObject) {
+                if (factoryPlanner != null) { // Проверка на null
+                    factoryPlanner.notifyAboutCarSale();
+                    factoryPlannerSynchronizationObject.notifyAll();
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
-        interrupt();
     }
 }
