14df7002d8b33e4ff7262fab819e87e493dd98a4
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-05-13 09:58:50 +0700

    Update Emulator.java

--- oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/Emulator.java
+++ oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/Emulator.java
@@ -7,29 +7,30 @@ import ru.nsu.avdonina.model.products.Body;
 import ru.nsu.avdonina.model.products.Car;
 import ru.nsu.avdonina.model.products.Engine;
 import ru.nsu.avdonina.model.storage.ProductStorage;
-import ru.nsu.avdonina.model.work.WorkersThreadPool;
-import ru.nsu.avdonina.model.work.carsale.Dealer;
-import ru.nsu.avdonina.model.work.carsale.DealersCreator;
+import ru.nsu.avdonina.model.work.ThreadPool;
+import ru.nsu.avdonina.model.work.TaskQueue;
+import ru.nsu.avdonina.model.work.Worker;
+import ru.nsu.avdonina.model.work.WorkersCreator;
 import ru.nsu.avdonina.model.work.factory.CarStorageFactoryPlanner;
 import ru.nsu.avdonina.model.work.factory.Factory;
-import ru.nsu.avdonina.model.work.factory.FactoryPlanner;
 import ru.nsu.avdonina.model.work.factory.FactoryWorkersCreator;
 import ru.nsu.avdonina.model.work.supply.*;
+import ru.nsu.avdonina.model.work.carsale.*;
 
 import java.util.logging.Logger;
 
 public class Emulator {
-    // Потоки поставщиков кузовов, двигателей и аксессуаров
-    private final WorkersThreadPool<BodySupplier> bodySuppliersPool;
-    private final WorkersThreadPool<EngineSupplier> engineSuppliersPool;
-    private final WorkersThreadPool<AccessorySupplier> accessorySuppliersPool;
-    // Поток работников фабрики
+    private ThreadPool bodySuppliersPool;
+    private ThreadPool engineSuppliersPool;
+    private ThreadPool accessorySuppliersPool;
+    private ThreadPool dealersPool;
     private final Factory factory;
-    // Поток дилеров
-    private final WorkersThreadPool<Dealer> dealersPool;
-    // Планировщик задач на фабрике (говорит фабрике, когда нужно выдать задания)
-    private final FactoryPlanner factoryPlanner;
+    private final CarStorageFactoryPlanner factoryPlanner;
     private final View view;
+    private final BodySuppliersCreator bodySuppliersCreator;
+    private final EngineSuppliersCreator engineSuppliersCreator;
+    private final AccessorySuppliersCreator accessorySuppliersCreator;
+    private final DealersCreator dealersCreator;
 
     public Emulator(Config config,
                     Logger logger,
@@ -40,83 +41,36 @@ public class Emulator {
                     StorageListener storageListener,
                     FactoryTasksListener factoryTasksListener) {
         this.view = view;
-        // Создание хранилищ продуктов
         final ProductStorage<Body> bodyStorage = new ProductStorage<>(config.bodyStorageCapacity(), storageListener);
         final ProductStorage<Engine> engineStorage = new ProductStorage<>(config.engineStorageCapacity(), storageListener);
         final ProductStorage<Accessory> accessoryStorage = new ProductStorage<>(config.accessoryStorageCapacity(), storageListener);
         final ProductStorage<Car> carStorage = new ProductStorage<>(config.carStorageCapacity(), storageListener);
-        // Создание объектов, которые будут создавать поставщиков
-        final BodySuppliersCreator bodySuppliersCreator = new BodySuppliersCreator(
-                bodyStorage,
-                supplyListener,
-                config.bodySupplyTimeMs()
-        );
-        final EngineSuppliersCreator engineSuppliersCreator = new EngineSuppliersCreator(
-                engineStorage,
-                supplyListener,
-                config.engineSupplyTimeMs()
 
-        );
-        final AccessorySuppliersCreator accessorySuppliersCreator = new AccessorySuppliersCreator(
-                accessoryStorage,
-                supplyListener,
-                config.accessorySupplyTimeMs()
-        );
-        // Создание пулов потоков поставщиков
-        this.bodySuppliersPool = new WorkersThreadPool<>(
-                bodySuppliersCreator,
-                config.bodySuppliersCount()
-        );
-        this.engineSuppliersPool = new WorkersThreadPool<>(
-                engineSuppliersCreator,
-                config.engineSuppliersCount()
-        );
-        this.accessorySuppliersPool = new WorkersThreadPool<>(
-                accessorySuppliersCreator,
-                config.accessorySuppliersCount()
-        );
-        // Создание объектов, которые будут создавать работников фабрики
-        final FactoryWorkersCreator factoryWorkerWorkersCreator = new FactoryWorkersCreator(
-                bodyStorage,
-                engineStorage,
-                accessoryStorage,
-                carStorage,
-                carAssembleListener,
-                factoryTasksListener,
-                config.carAssemblingTimeMs()
-        );
-        // Создание фабрики (которая управляет сборкой машин)
-        this.factory = new Factory(
-                factoryWorkerWorkersCreator,
-                factoryTasksListener, config.workersCount()
-        );
-        // Планировщик задач фабрики, ориентированный на свободное место на складе машин
+        this.bodySuppliersCreator = new BodySuppliersCreator(bodyStorage, supplyListener, config.bodySupplyTimeMs());
+        this.engineSuppliersCreator = new EngineSuppliersCreator(engineStorage, supplyListener, config.engineSupplyTimeMs());
+        this.accessorySuppliersCreator = new AccessorySuppliersCreator(accessoryStorage, supplyListener, config.accessorySupplyTimeMs());
+        final FactoryWorkersCreator factoryWorkerCreator = new FactoryWorkersCreator(
+                bodyStorage, engineStorage, accessoryStorage, carStorage,
+                carAssembleListener, factoryTasksListener, config.carAssemblingTimeMs());
+        this.factory = new Factory(factoryWorkerCreator, factoryTasksListener, config.workersCount());
         this.factoryPlanner = new CarStorageFactoryPlanner(factory, carStorage);
-        // Создание объектов, которые создают дилеров
-        final DealersCreator dealersCreator = new DealersCreator(
-                carStorage,
-                carRequestListener,
-                factoryPlanner,
-                logger,
-                config.carRequestTimeMs()
-        );
-        // Создание пула потоков дилеров
-        this.dealersPool = new WorkersThreadPool<>(
-                dealersCreator,
-                config.dealersCount()
-        );
+        this.dealersCreator = new DealersCreator(carStorage, carRequestListener, factoryPlanner, logger, config.carRequestTimeMs());
+        this.bodySuppliersPool = new ThreadPool(config.bodySuppliersCount(), new TaskQueue(1000));
+        this.engineSuppliersPool = new ThreadPool(config.engineSuppliersCount(), new TaskQueue(1000));
+        this.accessorySuppliersPool = new ThreadPool(config.accessorySuppliersCount(), new TaskQueue(1000));
+        this.dealersPool = new ThreadPool(config.dealersCount(), new TaskQueue(1000));
     }
-    // Методы изменения параметров во время работы
+
     public void setBodySupplyTime(int newValue) {
-        bodySuppliersPool.setWorkTime(newValue);
+        bodySuppliersCreator.setWorkTime(newValue);
     }
 
     public void setEngineSupplyTime(int newValue) {
-        engineSuppliersPool.setWorkTime(newValue);
+        engineSuppliersCreator.setWorkTime(newValue);
     }
 
     public void setAccessorySupplyTime(int newValue) {
-        accessorySuppliersPool.setWorkTime(newValue);
+        accessorySuppliersCreator.setWorkTime(newValue);
     }
 
     public void setCarAssembleTime(int newValue) {
@@ -124,19 +78,25 @@ public class Emulator {
     }
 
     public void setCarRequestTime(int newValue) {
-        dealersPool.setWorkTime(newValue);
+        dealersCreator.setWorkTime(newValue);
     }
 
     public void setBodySuppliersCount(int newValue) {
-        bodySuppliersPool.setWorkersCount(newValue);
+        bodySuppliersPool.shutdown();
+        bodySuppliersPool = new ThreadPool(newValue, new TaskQueue(1000));
+        submitSupplierTasks(bodySuppliersCreator, bodySuppliersPool, newValue);
     }
 
     public void setEngineSuppliersCount(int newValue) {
-        engineSuppliersPool.setWorkersCount(newValue);
+        engineSuppliersPool.shutdown();
+        engineSuppliersPool = new ThreadPool(newValue, new TaskQueue(1000));
+        submitSupplierTasks(engineSuppliersCreator, engineSuppliersPool, newValue);
     }
 
     public void setAccessorySuppliersCount(int newValue) {
-        accessorySuppliersPool.setWorkersCount(newValue);
+        accessorySuppliersPool.shutdown();
+        accessorySuppliersPool = new ThreadPool(newValue, new TaskQueue(1000));
+        submitSupplierTasks(accessorySuppliersCreator, accessorySuppliersPool, newValue);
     }
 
     public void setWorkersCount(int newValue) {
@@ -144,24 +104,64 @@ public class Emulator {
     }
 
     public void setDealersCount(int newValue) {
-        dealersPool.setWorkersCount(newValue);
+        dealersPool.shutdown();
+        dealersPool = new ThreadPool(newValue, new TaskQueue(1000));
+        submitDealerTasks(dealersCreator, dealersPool, newValue);
     }
 
-    public void start() { // Запуск всей симуляции
-        bodySuppliersPool.start();
-        engineSuppliersPool.start();
-        accessorySuppliersPool.start();
-        dealersPool.start();
+    public void start() {
+        submitSupplierTasks(bodySuppliersCreator, bodySuppliersPool, bodySuppliersPool.getWorkersCount());
+        submitSupplierTasks(engineSuppliersCreator, engineSuppliersPool, engineSuppliersPool.getWorkersCount());
+        submitSupplierTasks(accessorySuppliersCreator, accessorySuppliersPool, accessorySuppliersPool.getWorkersCount());
+        submitDealerTasks(dealersCreator, dealersPool, dealersPool.getWorkersCount());
         factoryPlanner.start();
-        factory.start();
     }
 
-    public void terminate() { // Остановка всей симуляции
-        bodySuppliersPool.terminate();
-        engineSuppliersPool.terminate();
-        accessorySuppliersPool.terminate();
-        dealersPool.terminate();
-        factory.terminate();
+private void submitSupplierTasks(WorkersCreator<?> creator, ThreadPool pool, int count) {
+    for (int i = 0; i < count; i++) {
+        Runnable task = createSupplierTask(creator, pool);
+        pool.execute(task);
+    }
+}
+
+    private Runnable createSupplierTask(WorkersCreator<?> creator, ThreadPool pool) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                Worker worker = creator.createWorker();
+                worker.run();
+                if (!Thread.currentThread().isInterrupted()) {
+                    pool.execute(this); // Пересоздаём текущую задачу
+                }
+            }
+        };
+    }
+    private void submitDealerTasks(DealersCreator creator, ThreadPool pool, int count) {
+        for (int i = 0; i < count; i++) {
+            Runnable task = createDealerTask(creator, pool);
+            pool.execute(task);
+        }
+    }
+
+    private Runnable createDealerTask(DealersCreator creator, ThreadPool pool) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                Dealer dealer = creator.createWorker();
+                dealer.run();
+                if (!Thread.currentThread().isInterrupted()) {
+                    pool.execute(this); // Пересоздаём текущую задачу
+                }
+            }
+        };
+    }
+
+    public void terminate() {
+        bodySuppliersPool.shutdown();
+        engineSuppliersPool.shutdown();
+        accessorySuppliersPool.shutdown();
+        dealersPool.shutdown();
+        factory.shutdown();
         factoryPlanner.interrupt();
         view.close();
     }
