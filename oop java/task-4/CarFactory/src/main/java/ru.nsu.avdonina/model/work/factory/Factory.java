0aa368af7282918fb6215f7ff375b0a2d0d01fb4
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-05-13 09:52:47 +0700

    Update Factory.java

--- oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/factory/Factory.java
+++ oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/factory/Factory.java
@@ -1,25 +1,40 @@
 package ru.nsu.avdonina.model.work.factory;
 
 import ru.nsu.avdonina.model.listeners.FactoryTasksListener;
-import ru.nsu.avdonina.model.work.WorkersThreadPool;
+import ru.nsu.avdonina.model.work.TaskQueue;
+import ru.nsu.avdonina.model.work.ThreadPool;
 
-public class Factory extends WorkersThreadPool<FactoryWorker> {
-    private final Object workersSynchronizationObject;
+public class Factory {
+    private ThreadPool threadPool;
+    private TaskQueue taskQueue;
+    private final FactoryWorkersCreator creator;
     private final FactoryTasksListener factoryTasksListener;
 
-    public Factory(FactoryWorkersCreator creator,
-                   FactoryTasksListener factoryTasksListener,
-                   int workersCount) {
-        super(creator, workersCount);
-        this.workersSynchronizationObject = creator.getWorkersSynchronizationObject();
+    public Factory(FactoryWorkersCreator creator, FactoryTasksListener factoryTasksListener, int workersCount) {
+        this.creator = creator;
         this.factoryTasksListener = factoryTasksListener;
+        this.taskQueue = new TaskQueue(1000); // Произвольная вместимость
+        this.threadPool = new ThreadPool(workersCount, taskQueue);
     }
 
     public void addTasks(int count) {
-        FactoryWorker.addTasks(count);
-        factoryTasksListener.notifyAboutNewTasks(count);
-        synchronized (workersSynchronizationObject) {
-            workersSynchronizationObject.notifyAll();
+        for (int i = 0; i < count; i++) {
+            threadPool.execute(creator.createWorker());
+            factoryTasksListener.notifyAboutNewTasks(1);
         }
+    }
+
+    public void setWorkTime(int workTime) {
+        creator.setWorkTime(workTime);
+    }
+
+    public void setWorkersCount(int newCount) {
+        threadPool.shutdown();
+        taskQueue = new TaskQueue(1000);
+        threadPool = new ThreadPool(newCount, taskQueue);
+    }
+
+    public void shutdown() {
+        threadPool.shutdown();
     }
 }
