e83fc17016556e735bd75992e33163b351d6e453
Avdonina Lada <144753760+avd0nina@users.noreply.github.com>
2025-05-13 09:51:08 +0700

    Update DealersCreator.java

--- oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/carsale/DealersCreator.java
+++ oop java/task-4/CarFactory/src/main/java/ru.nsu.avdonina/model/work/carsale/DealersCreator.java
@@ -1,3 +1,37 @@
+//package ru.nsu.avdonina.model.work.carsale;
+//
+//import ru.nsu.avdonina.model.listeners.CarRequestListener;
+//import ru.nsu.avdonina.model.products.Car;
+//import ru.nsu.avdonina.model.storage.Storage;
+//import ru.nsu.avdonina.model.work.WorkersCreator;
+//import ru.nsu.avdonina.model.work.factory.FactoryPlanner;
+//
+//import java.util.logging.Logger;
+//
+//public class DealersCreator extends WorkersCreator<Dealer> { // фабрика для создания потоков-продавцов автомобилей
+//    private final Storage<Car> storage;
+//    private final CarRequestListener carRequestListener;
+//    private final FactoryPlanner factoryPlanner;
+//    private final Logger saleLogger;
+//
+//    public DealersCreator(Storage<Car> storage,
+//                          CarRequestListener carRequestListener,
+//                          FactoryPlanner factoryPlanner,
+//                          Logger saleLogger,
+//                          int workTime) {
+//
+//        super(workTime);
+//        this.storage = storage;
+//        this.carRequestListener = carRequestListener;
+//        this.factoryPlanner = factoryPlanner;
+//        this.saleLogger = saleLogger;
+//    }
+//
+//    @Override
+//    public Dealer createWorker() {
+//        return new Dealer(storage, carRequestListener, factoryPlanner, saleLogger, getWorkTime());
+//    }
+//}
 package ru.nsu.avdonina.model.work.carsale;
 
 import ru.nsu.avdonina.model.listeners.CarRequestListener;
@@ -8,7 +42,7 @@ import ru.nsu.avdonina.model.work.factory.FactoryPlanner;
 
 import java.util.logging.Logger;
 
-public class DealersCreator extends WorkersCreator<Dealer> { // фабрика для создания потоков-продавцов автомобилей
+public class DealersCreator extends WorkersCreator<Dealer> {
     private final Storage<Car> storage;
     private final CarRequestListener carRequestListener;
     private final FactoryPlanner factoryPlanner;
@@ -19,7 +53,6 @@ public class DealersCreator extends WorkersCreator<Dealer> { // фабрика �
                           FactoryPlanner factoryPlanner,
                           Logger saleLogger,
                           int workTime) {
-
         super(workTime);
         this.storage = storage;
         this.carRequestListener = carRequestListener;
